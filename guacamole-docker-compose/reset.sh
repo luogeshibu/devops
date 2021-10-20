8eae68fc04883b5afdfe20db7ad4cdcd7db4a182
luogeshibu <46916737+luogeshibu@users.noreply.github.com>
2021-10-20 16:21:51 +0800

    Update reset.sh

--- guacamole-docker-compose/reset.sh
+++ guacamole-docker-compose/reset.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 echo "This will delete your existing database (./data/)"
-echo "          delete your recordings        (./record/)"
-echo "          delete your drive files       (./drive/)"
-echo "          delete your certs files       (./nginx/ssl/)"
+echo "This will delete your existing containers (guacamole guacd postgres)"
 echo ""
 read -p "Are you sure? " -n 1 -r
 echo ""   # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then # do dangerous stuff
- chmod -R +x -- ./init
- sudo rm -r -f ./data/
+ docker-compose down
+ sudo rm -r -f ./data/  ./init/
 fi
+
 
